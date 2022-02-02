a09a7be5a17dbb104eeabfe9d650628e2890da26
Raffas Siela <raffas.siela@gmail.com>
2022-02-02 23:29:04 +0100

    actualisations

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,3 +1,26 @@
+/*Zadanie 5
+        Test steps:
+
+        Fill First name
+        Fill Last name
+        Fill Email
+        Choose a random 'Sex' radiobutton
+        Fill age
+        Choose a random 'Year of experience' radiobutton
+        Choose 'Automation Tester' profession
+        Select random option from 'Continents'
+        Select "Switch Commands" and "Wait Commands" from 'Selenium Commands'
+        Upload some File
+        Click 'Sign in' button
+        Check that "Form send with success" text has been shown
+        Gratis to tego zadania:
+        Poszukajcie jak ustawić domyślny folder pobierania w chromedriverze
+        Ustawcie wybrany przez was folder
+        'Policzcie' przy pomocy javy jaka jest ilość plików w tym folderze
+        kliknijcie w formularzu przycisk 'Test File to Download'
+        Sprawdźcie czy ilość plików w folderze jest taka jak wcześniej +1
+        Dla chętnych: możecie spróbować pobrać listę plików w danym folderze i sprawdzić czy zawiera ona ten plik test-file-to-download.xlsx*/
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
---
+++ src/test/java/URLTest.java
@@ -0,0 +1,22 @@
+/*zadanie 3:
+        Stoworzyć projekt o nazwie SeleniumBasic
+        Dodać TestBase -> jak na prezentacji
+        uzupełnić pom -> jak na prezentacji
+        zrobić test który wchodzi na https://seleniumui.moderntester.pl/
+        a następna sprawdza asercją czy URL otwarty w przeglądarce jest równy "https://seleniumui.moderntester.pl/"*/
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class URLTest extends TestBase{
+
+    @Test
+    public void shouldLogin(){
+        driver.get("https://seleniumui.moderntester.pl/");
+        String pageUrl = driver.getCurrentUrl();
+        System.out.println("Current Url is:"+ pageUrl);
+        Assert.assertEquals(pageUrl, "https://seleniumui.moderntester.pl/");
+    }
+}
+
+
